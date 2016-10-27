655583691912ed512d766acdcadeaa2ade34b2a8
Ralf Merz <ralf.merz@inm.ch>
2016-10-27 20:26:07 +0200

    do not process pages if hidden, deleted or doktype not 1

--- Classes/Service/VarnishCacheService.php
+++ Classes/Service/VarnishCacheService.php
@@ -25,7 +25,6 @@
 
 namespace Mittwald\Varnishcache\Service;
 
-
 use Mittwald\Varnishcache\Domain\Model\Server;
 use Mittwald\Varnishcache\Domain\Model\SysDomain;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
@@ -33,7 +32,8 @@ use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
-class VarnishCacheService {
+class VarnishCacheService
+{
 
     /**
      * @var \Mittwald\Varnishcache\Service\FrontendUrlGenerator
@@ -48,12 +48,24 @@ class VarnishCacheService {
     protected $domainRepository;
 
     /**
+     * @var \TYPO3\CMS\Frontend\Page\PageRepository
+     * @inject
+     */
+    protected $pageRepository;
+
+    /**
      * Flush cache for every found domain and given varnish server
      *
      * @param $currentPageId
      * @return bool
      */
-    public function flushCache($currentPageId) {
+    public function flushCache($currentPageId)
+    {
+
+        $currentPage = $this->pageRepository->getPage($currentPageId);
+        if (is_array($currentPage) && ($currentPage['deleted'] == 1 || $currentPage['hidden'] == 1) || $currentPage['doktype'] != 1) {
+            return false;
+        }
 
         $url = $this->frontendUrlGenerator->getFrontendUrl($currentPageId);
 
@@ -71,13 +83,11 @@ class VarnishCacheService {
                     foreach ($servers as $server) {
                         $this->request($domain, $server, $url);
                     }
-
-
                 }
             }
-            return TRUE;
+            return true;
         }
-        return FALSE;
+        return false;
     }
 
     /**
@@ -88,7 +98,8 @@ class VarnishCacheService {
      * @param $frontendUrl
      * @return mixed
      */
-    protected function request(SysDomain $domain, Server $server, $frontendUrl) {
+    protected function request(SysDomain $domain, Server $server, $frontendUrl)
+    {
 
         if (!function_exists('curl_version')) {
             throw new \BadFunctionCallException('Curl is required but not loaded', '1444895510');
@@ -104,29 +115,31 @@ class VarnishCacheService {
         curl_setopt($curl, CURLOPT_NOBODY, 1);
         curl_setopt($curl, CURLOPT_PORT, $server->getPort());
         curl_setopt($curl, CURLOPT_URL, $server->getRequestUrlByFrontendUrl($frontendUrl));
-        curl_setopt($curl, CURLOPT_HTTP_VERSION, ($server->getProtocol() == 1) ? CURL_HTTP_VERSION_1_0 : CURL_HTTP_VERSION_1_1);
+        curl_setopt($curl, CURLOPT_HTTP_VERSION,
+            ($server->getProtocol() == 1) ? CURL_HTTP_VERSION_1_0 : CURL_HTTP_VERSION_1_1);
 
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $server->getMethod());
 
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-                'X-Host: ' . $domain->getDomainName(),
-                'X-Url: ' . $frontendUrl,
+            'X-Host: ' . $domain->getDomainName(),
+            'X-Url: ' . $frontendUrl,
         ));
         curl_setopt($curl, CURLINFO_HEADER_OUT, 1);
         $header = curl_exec($curl);
         curl_close($curl);
 
-        $this->getBackendUser()->writelog(3, 1, 0, 0, 'User %s has cleared the varnishcache (server=%s,host=%s, url=%s)', array($this->getBackendUser()->user['username'], $server->getIp(), $domain->getDomainName(), $frontendUrl));
+        $this->getBackendUser()->writelog(3, 1, 0, 0,
+            'User %s has cleared the varnishcache (server=%s,host=%s, url=%s)',
+            array($this->getBackendUser()->user['username'], $server->getIp(), $domain->getDomainName(), $frontendUrl));
 
         return $header;
-
     }
-
 
     /**
      * @return BackendUserAuthentication
      */
-    protected function getBackendUser() {
+    protected function getBackendUser()
+    {
         return $GLOBALS['BE_USER'];
     }
 }
